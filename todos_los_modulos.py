35ef682bc122a9f6b360d95326264c5bbe0ef976
Borja Garibotti <martugaribotti@hotmail.com>
2020-05-22 17:20:22 -0300

    update dbconf.py y db.py->ecommerce_db.py

--- todos_los_modulos.py
+++ todos_los_modulos.py
@@ -4,7 +4,6 @@ from datetime import date, datetime
 from getpass import getpass
 from validate_email import validate_email
 from modulos.db import Ecommerce_db
-from modulos.dbconf import conf, queries
 from modulos.usuarios import Usuario, encriptar
 from modulos.productos import Producto
 from modulos.compras import Compra
